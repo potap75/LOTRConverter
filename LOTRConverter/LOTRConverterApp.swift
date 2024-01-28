3e707d6106c82b9a90bb8a9394ff87af3e38e9b6
potap75 <rummaan@me.com>
2024-01-27 23:49:45 -0500

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Roman Potapov on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
